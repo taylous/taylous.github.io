843608164f8d95794f4423109a8a0db4d1b68f9a
Taylous <taylous99@gmail.com>
2025-03-23 21:22:33 +0900

    배포에 필요한 .yml 파일 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'taylous', // Usually your GitHub org/user name.
   projectName: 'taylous.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
